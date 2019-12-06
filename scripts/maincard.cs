5611663ff3ce3ef007db6293dee33cf3647afc68
tovernaar123 <56041037+tovernaar123@users.noreply.github.com>
2019-12-06 18:44:43 +0100

    main

---
+++ scripts/maincard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maincard : MonoBehaviour
+{
+    [SerializeField] private GameObject Card_back;
+    public void OnMouseDown()
+    {
+        if (Card_back.activeSelf)
+        {
+            Card_back.SetActive(false);
+        }else
+        {
+            Card_back.SetActive(true);
+        }
+    }
+}
